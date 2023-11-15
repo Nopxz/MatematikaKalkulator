8f70da6456fd808be3cb952e20891146db78e05c
naufalgitam <naufalps24@gmail.com>
2023-11-15 11:40:03 +0700

    Add project files.

---
+++ MatematikaKalkulator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+
+
+namespace MatematikaKalkulator
+{
+    public class Calculator
+    {
+        public string User(String name)
+        {
+            return name;
+        }
+
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ MatematikaKalkulatorTests/MatematikaTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatematikaKalkulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikaKalkulator.Tests
+{
+    [TestClass()]
+    public class MatematikaTests
+    {
+        [TestMethod()]
+        public void UserTest()
+        {
+            Calculator mtk = new Calculator();
+            Assert.AreEqual("Naufal Gita M", mtk.User("Naufal Gita M"));
+        }
+
+        [TestMethod()]
+        public void TambahTest()
+        {
+            Calculator mtk = new Calculator();
+            Assert.AreEqual(20, mtk.Tambah(10,10));
+        }
+
+        [TestMethod()]
+        public void KurangTest()
+        {
+            Calculator mtk = new Calculator();
+            Assert.AreEqual(20, mtk.Kurang(30, 10));
+        }
+    }
+}
